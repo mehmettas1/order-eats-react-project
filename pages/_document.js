b427206c097540a75fb907f92a283f90f7f35284
mehmet <tas349873@gmail.com>
2023-05-28 18:51:50 +0300

    react-icons

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        
+               {/* <link
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Open+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        /> */}
       </Head>
       <body>
         <Main />
