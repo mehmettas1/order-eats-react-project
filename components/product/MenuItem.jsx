255dcebb22c523c5b998c63065cfb1e2227577c9
mehmet <tas349873@gmail.com>
2023-06-02 17:04:57 +0300

    menu-item-update

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,11 +1,21 @@
 import Image from 'next/image'
-import React from 'react'
-
+import { RiShoppingCart2Fill } from "react-icons/ri";
 const MenuItem = () => {
   return (
-<div className="">
-    <div className="relative w-40 h-40 ">
-        <Image src="/images/f1.png" alt='' layout='fill'/>
+<div className="bg-secondary rounded-3xl">
+  <div className="w-full bg-[#f1f2f3] h-52 grid place-content-center rounded-bl-[46px] rounded-tr-2xl rounded-tl-2xl">
+  <div className="relative  w-36 h-36 hover:scale-105 transition-all ">
+        <Image className=' ' src="/images/f1.png" alt='' layout='fill'/>
+    </div>
+  </div>
+    <div className="text-white p-6 ">
+        <h4 className='text-xl font-semibold'>Delicious Pizza</h4>
+        <p className=" text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolores harum officia autem doloremque .</p>
+        <div className="flex justify-between ">
+
+        <span className='text-white' >$20</span>
+        <button className=" btn-primary h-10 w-10 rounded-full p-0  grid place-content-center"><RiShoppingCart2Fill/></button>
+  </div>
     </div>
 </div>
   )
--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -3,7 +3,9 @@ import MenuItem from './MenuItem'
 
 const MenuWrapper = () => {
   return (
-    <div className="container mx-auto flex flex-col items-center justify-center mb-16">
+    <div className="container mx-auto ">
+        <div className="flex flex-col items-center justify-center mb-16">
+
         <Title className="text-[40px]">Our Menu</Title>
         <div className="mt-10">
             <button className='px-7 py-2 rounded-3xl text-white bg-secondary'>All</button>
@@ -12,8 +14,13 @@ const MenuWrapper = () => {
             <button className='px-7 py-2 rounded-3xl  '>Drink</button>
 
         </div>
-        <div className="mt-8">
+        </div>
+     
+        <div className="mt-8 grid sm:grid-cols-2  md:grid-cols-3 gap-4 grid-cols-1">
             <MenuItem/>
+            <MenuItem/>
+            <MenuItem/>
+            
         </div>
     </div>
   )
