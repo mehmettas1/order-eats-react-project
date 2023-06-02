123516ab95cb4e1a2e3c1c52775ec7bee22fdca4
mehmet <tas349873@gmail.com>
2023-06-02 14:54:54 +0300

    carousel

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,8 +1,32 @@
 import Image from "next/image";
 import React from "react";
 import Title from "./ui/Title";
+import Slider from "react-slick";
 
 const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    appenDots: (dots) => (
+      <div
+        style={{
+          color: "yellow",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ marginTop: "10px" }}>{dots}</ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div className="w-3 h-3 border bg-white rounded-full mt-10"></div>
+    ),
+  };
   return (
     <div className="h-screen w-full container mx-auto -mt-[88px]">
       <div className="absolute top-0 left-0 w-full h-full">
@@ -15,16 +39,41 @@ const Carousel = () => {
           />
         </div>
       </div>
-      <div className="relative text-white top-48 flex flex-col items-start gap-y-10">
-        <Title addClass="text-6xl">Fast Food Restaurant</Title>
-        <p className="text-sm sm:w-2/5 w-full">
-          Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente
-          ad mollitia laborum quam quisquam esse error unde. Tempora ex
-          doloremque, labore, sunt repellat dolore, iste magni quos nihil
-          ducimus libero ipsam.
-        </p>
-        <button className="btn-primary">Order Now</button>
-      </div>
+      <Slider {...settings}>
+        <div className="">
+          <div className="mt-48 text-white  flex flex-col items-start gap-y-10">
+            <Title addClass="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm sm:w-2/5 w-full">
+              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+              sapiente ad mollitia laborum quam quisquam esse error unde.
+              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
+              quos nihil ducimus libero ipsam.
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+        <div className="">
+          <div className=" text-white mt-48 flex flex-col items-start gap-y-10">
+            <Title addClass="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm sm:w-2/5 w-full">
+              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+              sapiente ad mollitia laborum quam quisquam esse error unde.
+              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
+              quos nihil ducimus libero ipsam.
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+        <div className="">
+          <div className=" text-white mt-48 flex flex-col items-start gap-y-10">
+            <Title addClass="text-6xl">Fast Food Restaurant</Title>
+            <p className="text-sm sm:w-2/5 w-full">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas blanditiis nulla eveniet repudiandae excepturi placeat commodi libero aliquid. Vel magnam sapiente ducimus nemo molestias est, assumenda saepe repellendus illo excepturi exercitationem nam  
+            </p>
+            <button className="btn-primary">Order Now</button>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 };
