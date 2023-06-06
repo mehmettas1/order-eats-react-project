a4c8193ae9d10aa2e9aadd3c45847b169138e782
mehmet <tas349873@gmail.com>
2023-06-06 10:16:27 +0300

    yup-update

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -2,28 +2,26 @@ import React from "react";
 import Input from "./form/Input";
 import Title from "./ui/Title";
 import { useFormik } from "formik";
-import { rezarvatipSchema } from "@/schema/reservation";
+import { reservationSchema } from "../schema/reservation";
 
 const Reservation = () => {
-
-const onSubmit = async(values,actions) =>{
-    await new Promise((resolve)=> setTimeout(resolve,400));
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
     actions.resetForm();
-}
+  };
 
-
-
-  const {values,errors, handleSubmit,handleChange} = useFormik({
-    initialValues: {
-      fullName: "",
-      phoneNumber: "",
-      email: "",
-      persons: "",
-      date: "",
-    },
-    onSubmit,
-    validationSchema: rezarvatipSchema,
-  });
+  const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
+    useFormik({
+      initialValues: {
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        persons: "",
+        date: "",
+      },
+      onSubmit,
+      validationSchema: reservationSchema,
+    });
 
   const inputs = [
     {
@@ -32,7 +30,8 @@ const onSubmit = async(values,actions) =>{
       type: "text",
       placeholder: "Your Full Name",
       value: values.fullName,
-      errorMessage:errors.FullName,
+      errorMessage: errors.fullName,
+      touched: touched.fullName,
     },
     {
       id: 2,
@@ -40,7 +39,8 @@ const onSubmit = async(values,actions) =>{
       type: "number",
       placeholder: "Your Phone Number",
       value: values.phoneNumber,
-      
+      errorMessage: errors.phoneNumber,
+      touched: touched.phoneNumber,
     },
     {
       id: 3,
@@ -48,6 +48,8 @@ const onSubmit = async(values,actions) =>{
       type: "email",
       placeholder: "Your Email Address",
       value: values.email,
+      errorMessage: errors.email,
+      touched: touched.email,
     },
     {
       id: 4,
@@ -55,12 +57,16 @@ const onSubmit = async(values,actions) =>{
       type: "number",
       placeholder: "How Many Persons?",
       value: values.persons,
+      errorMessage: errors.persons,
+      touched: touched.persons,
     },
     {
       id: 5,
       name: "date",
       type: "datetime-local",
       value: values.date,
+      errorMessage: errors.date,
+      touched: touched.date,
     },
   ];
 
@@ -71,12 +77,19 @@ const onSubmit = async(values,actions) =>{
         <form className="lg:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-              <Input key={input.id} {...input} onChange={handleChange} />
+              <Input
+                key={input.id}
+                {...input}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
             ))}
           </div>
-          <button type="submit" className="btn-primary mt-4">BOOK NOW</button>
+          <button className="btn-primary mt-4" type="submit">
+            BOOK NOW
+          </button>
         </form>
-        <div className="lg:flex-1  w-full">
+        <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48348.66924008447!2d-74.24927437205034!3d40.766603131286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254b5958982c3%3A0xb6ab3931055a2612!2sEast%20Orange%2C%20New%20Jersey%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1661853137161!5m2!1str!2str"
             allowFullScreen=""
--- schema/reservation.js
+++ schema/reservation.js
@@ -1,7 +1,13 @@
 import * as Yup from "yup";
 
-
-
-export const rezarvatipSchema =Yub.oblect({
-    fullName:Yub.string().required("Full name is required").min(3,"Full name must be at least 3 characters"),
-})
+export const reservationSchema = Yup.object({
+  fullName: Yup.string()
+    .required("Full name is required.")
+    .min(3, "Full name must be at least 3 characters."),
+  phoneNumber: Yup.string()
+    .required("Full name is required.")
+    .min(10, "Phone number must be at least 10 characters."),
+  email: Yup.string().required("Email is required.").email("Email is invalid."),
+  persons: Yup.string().required("Persons is required."),
+  date: Yup.string().required("Date is required."),
+});
