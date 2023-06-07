2949379bd1a8fb1417b71b39561959d6edf344db
mehmet <tas349873@gmail.com>
2023-06-07 15:11:00 +0300

    categories

--- components/admin/Category.jsx
+++ components/admin/Category.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import Title from '../ui/Title';
-import Input from '../form/Input';
+import React, { useState } from "react";
+import Title from "../ui/Title";
+import Input from "../form/Input";
 
 const Category = () => {
+  const [categories, setCategories] = useState(["pizza"]);
+  const [inputText, setInputText] = useState("");
   return (
-<div className="lg:p-8 flex-1 lg:mt-0 mt-5">
-    
+    <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
       <Title addClass="text-[40px]">Category</Title>
-   <div className="">
-    <div className="flex gap-4">
-        <Input/>
-        <button className="btn-primary">Add</button>
-    </div>
-   </div>
+      <div className="mt-5">
+        <div className="flex gap-4 items-center">
+          <Input
+            placeholder="Add a new Category..."
+            onChange={(e) => {
+              setInputText(e.target.value);
+            }}
+            value={inputText}
+          />
+          <button
+            className="btn-primary "
+            onClick={() => {
+              setCategories([...categories, inputText]);
+              setInputText("");
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div className="mt-10">
+        {categories.map((category, index) => (
+          <div className="flex justify-between items-center" key={index}>
+            <b className="text-xl">{category}</b>
+            <button
+              className="btn-primary !bg-danger"
+              onClick={() => {
+                setCategories(categories.filter((cat) => cat !== category));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
- 
-}
+};
 
-export default Category
+export default Category;
