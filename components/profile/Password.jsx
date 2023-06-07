d6c4ef2645413a00160944aef2d4a2090c786a10
mehmet <tas349873@gmail.com>
2023-06-07 14:04:20 +0300

    update

--- components/profile/Password.jsx
+++ components/profile/Password.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Title from "../ui/Title";
 import Input from "../form/Input";
 import { useFormik } from "formik";
+import { newPasswordSchema } from "@/schema/newPassword";
 
-import { profileSchema } from "../../schema/profile";
+
+
 
 const Password = () => {
   const onSubmit = async (values, actions) => {
@@ -14,86 +16,48 @@ const Password = () => {
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
     useFormik({
       initialValues: {
-        fullName: "",
-        phoneNumber: "",
-        email: "",
-        address: "",
-        job: "",
-        bio: "",
+        password: "",
+        confirmPassword: "",
+     
       },
       onSubmit,
-      validationSchema: profileSchema,
+      validationSchema: newPasswordSchema,
     });
   const inputs = [
     {
       id: 1,
-      name: "fullName",
-      type: "text",
-      placeholder: "Your Full Name",
-      value: values.fullName,
-      errorMessage: errors.fullName,
-      touched: touched.fullName,
+      name: "password",
+      type: "password",
+      placeholder: "Your password",
+      value: values.password,
+      errorMessage: errors.password,
+      touched: touched.password,
     },
     {
       id: 2,
-      name: "phoneNumber",
-      type: "number",
-      placeholder: "Your Phone Number",
-      value: values.phoneNumber,
-      errorMessage: errors.phoneNumber,
-      touched: touched.phoneNumber,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Your confirmPassword ",
+      value: values.confirmPassword,
+      errorMessage: errors.confirmPassword,
+      touched: touched.confirmPassword,
     },
-    {
-      id: 3,
-      name: "email",
-      type: "email",
-      placeholder: "Your Email Address",
-      value: values.email,
-      errorMessage: errors.email,
-      touched: touched.email,
-    },
-    {
-      id: 4,
-      name: "address",
-      type: "text",
-      placeholder: "Your Address",
-      value: values.address,
-      errorMessage: errors.address,
-      touched: touched.address,
-    },
-    {
-      id: 5,
-      name: "job",
-      type: "text",
-      placeholder: "Your Job",
-      value: values.job,
-      errorMessage: errors.job,
-      touched: touched.job,
-    },
-    {
-      id: 6,
-      name: "bio",
-      type: "text",
-      placeholder: "Your Bio",
-      value: values.bio,
-      errorMessage: errors.bio,
-      touched: touched.bio,
-    },
+   
   ];
   return (
-    <form className="lg:p-8 lg:mt-0 mt-5 flex-1">
+    <form className="lg:p-8 lg:mt-0 mt-5 flex-1" onSubmit={handleSubmit}>
       <Title addClass="text-[40px]">Password</Title>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
         {inputs.map((input) => (
           <Input
-            key="input.id"
+            key= {input.id}
             {...input}
             onBlur={handleBlur}
             onChange={handleChange}
           />
         ))}
       </div>
-      <button className="btn-primary mt-4">Update</button>
+      <button className="btn-primary mt-4" type="submit">Update</button>
     </form>
   );
 };
